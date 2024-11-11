f07de61854a6f2e097f06d2ec930b086b37befb4
xl <xl18845898715@gmail.com>
2024-11-11 22:07:57 +0800

    feat: 处理用户添加逻辑

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,37 @@
+const userService = require('../service/user.service');
+
 class UserController {
-  create(ctx, next) {
+  async create(ctx, next) {
     const user = ctx.request.body;
 
-    console.log(user);
+    const { username, password } = user;
 
-    ctx.body = '用户创建成功';
+    // 判断 参数是否传递
+    if (!username || !password) {
+      ctx.body = {
+        code: -1001,
+        message: '用户名或者密码不能为空',
+      };
+      return;
+    }
+
+    // 查询数据库中是否存在
+    const userInfo = await userService.findUserByName(username);
+
+    if (userInfo.length) {
+      ctx.body = {
+        code: -1002,
+        message: '用户名已经被占用！！！',
+      };
+      return;
+    }
+
+    const result = await userService.create(user);
+
+    ctx.body = {
+      message: '用户创建成功',
+      data: result,
+    };
   }
 }
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,21 @@
 const connection = require('../app/database');
 
 class UserService {
-  create(user) {}
+  async create(user) {
+    // 1. 获取用户
+    const { username, password } = user;
+    // 2. 拼接 sql
+    const statement = 'insert into `user` (name, password) values (?, ?)';
+    // 3. 执行 sql
+    const [result] = await connection.execute(statement, [username, password]);
+    return result;
+  }
+
+  async findUserByName(name) {
+    const statement = 'select * from `user` where name = ?';
+    const [values] = await connection.execute(statement, [name]);
+    return values;
+  }
 }
 
 module.exports = new UserService();
